477b2ef2f3a3b2e64069927db66ad59cade6817e
Đặng Nhật Anh <awmtrg21@gmail.com>
2025-03-22 14:26:17 +0700

    chore: Cập nhật cấu hình và assets cho ứng dụng

--- app.config.ts
+++ app.config.ts
@@ -9,17 +9,18 @@ module.exports = ({ config }: ConfigContext) => {
     return {
       ...config,
       extra: {
-        apiUrl: 'https://6669-14-232-206-135.ngrok-free.app',
-        fileUrl: 'https://1cfc-14-232-206-135.ngrok-free.app',
-        fileUser: 'cxxc',
-        filePassword: 'cxxc@123',
-        minio: {
-          accessKey: 'IxuuYWSIdf8XFpw6WvAD',
-          secretKey: 'jD4Z79wGwD1Va5DO4sO487LzUyl12vhwYQwWxthN',
-          useSSL: true,
-          endPoint: 'https://1cfc-14-232-206-135.ngrok-free.app',
-          port: 443
-        }
+        ...config.extra,
+        "apiUrl": "https://96ca-2401-d800-91c1-d3f1-c449-6648-7c0a-aee2.ngrok-free.app",
+        "fileUrl": "https://288b-2401-d800-91c1-d3f1-c449-6648-7c0a-aee2.ngrok-free.app",
+        "fileUser": "cxxc",
+        "filePassword": "cxxc@123",
+        "minio": {
+          "accessKey": "IxuuYWSIdf8XFpw6WvAD",
+          "secretKey": "jD4Z79wGwD1Va5DO4sO487LzUyl12vhwYQwWxthN",
+          "useSSL": true,
+          "endPoint": "https://288b-2401-d800-91c1-d3f1-c449-6648-7c0a-aee2.ngrok-free.app",
+          "port": 443
+        },
       }
     };
   }
