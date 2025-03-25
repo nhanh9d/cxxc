d9902005f7760dafb21c1da338740958ee1c844d
Đặng Nhật Anh <awmtrg21@gmail.com>
2025-03-25 08:56:24 +0700

    fix: adjust status bar color for Android

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,38 +1,43 @@
-import Header from "@/components/Header";
-import { ApiProvider } from "@/contexts/ApiContext";
-import { AuthProvider } from "@/contexts/AuthContext";
 import { Providers } from "@/contexts/Providers";
-import { ImageUploadProvider } from "@/contexts/UploadImageContext";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import { DarkTheme, DefaultTheme, ThemeProvider } from "@react-navigation/native";
-import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
-import { StatusBar } from "react-native";
+import { StatusBar, Platform } from "react-native";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
 
   useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
+    SplashScreen.hideAsync();
+  }, []);
 
   return (
     <Providers>
-      <StatusBar barStyle={colorScheme === 'dark' ? "light-content" : "dark-content"} />
-      <Stack />
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="#FFFCEE"
+        translucent={true}
+      />
+      <Stack
+        screenOptions={{
+          gestureEnabled: true, // đảm bảo cho phép gesture back
+          headerTitleStyle: {
+            fontFamily: Platform.select({
+              ios: 'System',
+              android: 'Roboto',
+            }),
+          },
+          headerBackTitleStyle: {
+            fontFamily: Platform.select({
+              ios: 'System',
+              android: 'Roboto',
+            }),
+          },
+        }}
+      />
     </Providers>
   );
 }
