6ac337affaa89c54019f45f8859b5d8ca0bfa0c9
Nha An <dnhanh95@gmail.com>
2025-05-28 10:46:18 +0700

    update package and adjust way to get push token

--- app/index.tsx
+++ app/index.tsx
@@ -2,12 +2,12 @@ import React, { useEffect } from "react";
 import { StyleSheet, Image } from "react-native";
 import { useNavigation, useRouter } from "expo-router";
 import { ThemedView } from "@/components/layout/ThemedView";
-import * as Notifications from 'expo-notifications';
 import { getToken, saveToken, removeToken } from "@/helpers/secureStore";
 import { useAuth } from "@/contexts/AuthContext";
 import { useApi } from "@/contexts/ApiContext";
 import Constants from "expo-constants";
 import { jwtDecode } from "jwt-decode";
+import messaging from "@react-native-firebase/messaging";
 
 export default function LoginScreen() {
   const router = useRouter();
@@ -22,24 +22,25 @@ export default function LoginScreen() {
   useEffect(() => {
     const checkToken = async (
     ) => {
-      const settings = await Notifications.getPermissionsAsync();
-      if (!settings.granted) {
-        const { status } = await Notifications.requestPermissionsAsync();
-        if (status !== 'granted') {
-          throw new Error('Permission not granted for notifications');
-        }
+      const status = await messaging().requestPermission();
+      const enabled =
+        status === messaging.AuthorizationStatus.AUTHORIZED ||
+        status === messaging.AuthorizationStatus.PROVISIONAL;
+
+      if (!enabled) {
+        return;
       }
 
-      const token = await Notifications.getDevicePushTokenAsync();
+      const token = await messaging().getToken();
       const storedPushToken = await getToken("pushToken");
       try {
-        await axios.put(`${Constants.expoConfig?.extra?.apiUrl}/user/push-token`, { pushToken: token.data });
+        await axios.put(`${Constants.expoConfig?.extra?.apiUrl}/user/push-token`, { pushToken: token });
       } catch (error) {
         console.log("Lỗi khi cập nhật push token:", error);
       }
-      
-      if (!storedPushToken || storedPushToken !== token.data) {
-        await saveToken("pushToken", token.data);
+
+      if (!storedPushToken || storedPushToken !== token) {
+        await saveToken("pushToken", token);
       }
     };
 
