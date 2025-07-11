87d13ad1310c536b89451d8432a2527d48a7754c
Andy Nguyen <andynguyen@Andys-MacBook-Pro.local>
2025-07-11 14:22:54 +0700

    add deps

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,32 @@
-import { Providers } from "@/contexts/Providers";
-import { useColorScheme } from "@/hooks/useColorScheme";
-import { useThemeColor } from "@/hooks/useThemeColor";
-import { SplashScreen, Stack } from "expo-router";
-import { useEffect } from "react";
-import { StatusBar, Platform } from "react-native";
-
+import { Providers } from '@/contexts/Providers'
+import { useColorScheme } from '@/hooks/useColorScheme'
+import { useThemeColor } from '@/hooks/useThemeColor'
+import { SplashScreen, Stack } from 'expo-router'
+import { useEffect } from 'react'
+import { StatusBar, Platform } from 'react-native'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
+GoogleSignin.configure({
+  iosClientId: '622023085823-46cm30rk1805ese8tlrne9eqch4nbk15.apps.googleusercontent.com', // [iOS] if you want to specify the client ID of type iOS (otherwise, it is taken from GoogleService-Info.plist)
+  profileImageSize: 120, // [iOS] The desired height (and width) of the profile image. Defaults to 120px
+})
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const backgroundColor = useThemeColor({ light: "#FFFCEE", dark: "#2B2A27" }, 'background');
+  const colorScheme = useColorScheme()
+  const backgroundColor = useThemeColor(
+    { light: '#FFFCEE', dark: '#2B2A27' },
+    'background'
+  )
 
   useEffect(() => {
-    SplashScreen.hideAsync();
-  }, []);
+    SplashScreen.hideAsync()
+  }, [])
 
   return (
     <Providers>
       <StatusBar
-        barStyle={colorScheme === "dark" ? "light-content" : "dark-content"}
+        barStyle={colorScheme === 'dark' ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundColor}
         translucent={true}
       />
@@ -29,17 +36,17 @@ export default function RootLayout() {
           headerTitleStyle: {
             fontFamily: Platform.select({
               ios: 'System',
-              android: 'Roboto',
-            }),
+              android: 'Roboto'
+            })
           },
           headerBackTitleStyle: {
             fontFamily: Platform.select({
               ios: 'System',
-              android: 'Roboto',
-            }),
-          },
+              android: 'Roboto'
+            })
+          }
         }}
       />
     </Providers>
-  );
+  )
 }
--- app/auth/google.ts
+++ app/auth/google.ts
@@ -1,18 +1,11 @@
-import * as Google from "expo-auth-session/providers/google";
-import auth from "@react-native-firebase/auth";
+import * as Google from 'expo-auth-session/providers/google'
+import auth from '@react-native-firebase/auth'
+import { GoogleSignin } from '@react-native-google-signin/google-signin'
 
 export default async function signInWithGoogle() {
-  const [request, response, promptAsync] = Google.useIdTokenAuthRequest({
-    clientId: "YOUR_GOOGLE_WEB_CLIENT_ID", // Replace with your Firebase Google Client ID
-  });
-
-  if (response?.type === "success") {
-    const { id_token } = response.params;
-    // const credential = GoogleAuthProvider.credential(id_token);
-    // await auth().signInWithCredential(id_token);
-    // await signInWithCredential(auth, credential);
-    alert("Google Sign-In successful!");
-  } else {
-    console.log("Google Sign-In failed", response);
-  }
-};
+  try {
+    await GoogleSignin.hasPlayServices()
+    const response = await GoogleSignin.signIn()
+    console.log('Google Sign-In Response:', response)
+  } catch (error) {}
+}
