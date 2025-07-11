0493350f472ab02e0693e3101b30ecb2afdd6372
Andy Nguyen <andynguyen@Andys-MacBook-Pro.local>
2025-07-11 15:23:05 +0700

    add login function

--- app/auth/apple.ts
+++ app/auth/apple.ts
@@ -1,24 +1,15 @@
-import auth from '@react-native-firebase/auth';
-import { appleAuth } from '@invertase/react-native-apple-authentication';
-
-async function onAppleButtonPress() {
-  // Start the sign-in request
-  const appleAuthRequestResponse = await appleAuth.performRequest({
-    requestedOperation: appleAuth.Operation.LOGIN,
-    requestedScopes: [appleAuth.Scope.FULL_NAME, appleAuth.Scope.EMAIL],
-  });
-
-  // Ensure Apple returned a user identityToken
-  if (!appleAuthRequestResponse.identityToken) {
-    throw new Error('Apple Sign-In failed - no identify token returned');
-  }
-
-  // Create a Firebase credential from the response
-  const { identityToken, nonce } = appleAuthRequestResponse;
-  const appleCredential = auth.AppleAuthProvider.credential(identityToken, nonce);
-
-  // Sign the user in with the credential
-  return auth().signInWithCredential(appleCredential);
+import * as AppleAuthentication from 'expo-apple-authentication'
+async function signInWithApple() {
+  try {
+    const credential = await AppleAuthentication.signInAsync({
+      requestedScopes: [
+        AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+        AppleAuthentication.AppleAuthenticationScope.EMAIL
+      ]
+    })
+    console.log('Apple Sign-In Credential:', credential)
+    // signed in
+  } catch (e) {}
 }
 
-export default onAppleButtonPress;
+export default signInWithApple
